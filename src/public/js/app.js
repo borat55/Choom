786462a6917cfbc38d453460ad6111999961b7c1
borat55 <boram8655@gmail.com>
2022-06-10 16:07:33 -0300

    #1 Chat With WEbSockets (creating a function for realtime chat)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,40 @@
-console.log("hello");
+const messageList = document.querySelector("ul");
+const messageForm = document.getElementById("message");
+const nicknameForm = document.getElementById("nickname");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function nameMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server. ✅")
+})
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.appendChild(li);
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server. ❌")
+})
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input")
+    socket.send(nameMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(nameMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit)
--- src/server.js
+++ src/server.js
@@ -12,9 +12,27 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`✅ Server is listening on localhost:${PORT}`);
-
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser ✅")
+    socket.on("close", () => console.log("Disconnected from the Browser. ❌"));
+    socket.on("message", (message) => {
+        const parsedMsg = JSON.parse(message.toString())
+        switch (parsedMsg.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${parsedMsg.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = parsedMsg.payload;
+                break;
+        }
+    });
+});
 
 server.listen(PORT, handleListen);
